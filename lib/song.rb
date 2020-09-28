28a896281013b5cbea65032739ec803e5036f72d
Crystal Norman <cnorm88@gmail.com>
2020-09-28 17:35:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,21 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize
+  end
+
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+  
+  def self.create
+    song = song.new
+    self.all << song
+    song
   end
 
 end
