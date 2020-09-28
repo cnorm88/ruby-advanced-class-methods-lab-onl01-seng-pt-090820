61d4be9f4397fbe4098335a9ed872f8ee58044da
Crystal Norman <cnorm88@gmail.com>
2020-09-28 18:10:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,11 +32,25 @@ class Song
     song
   end
   
-def self.find_by_name(name)
+  def self.find_by_name(name)
     song = self.new
     song.name = name
     @@all.find{|song| song.name == name}
-    
   end
 
+  def self.find_or_create_by_name(name)
+    new_song = self.find_by_name(name)
+    if new_song == nil
+        self.create_by_name(name)
+    else
+        return new_song
+    end
+  end
+
+def self.alphabetical
+  self.all.sort_by do |song|
+    song.name
+  end
+end
+ 
 end
